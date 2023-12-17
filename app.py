fac1a927107cf2d782b696392970106db75e8462
laveezah-noor <laveezahnoor@gmail.com>
2023-12-17 14:21:24 +0500

    Add code

---
+++ app.py
@@ -0,0 +1,43 @@
+# Math Wizard - A Simple Calculator
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Error: Cannot divide by zero!"
+
+def main():
+    print("Welcome to Math Wizard!")
+    print("Choose an operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+
+    choice = input("Enter your choice (1/2/3/4): ")
+
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    if choice == '1':
+        print(f"Result: {add(num1, num2)}")
+    elif choice == '2':
+        print(f"Result: {subtract(num1, num2)}")
+    elif choice == '3':
+        print(f"Result: {multiply(num1, num2)}")
+    elif choice == '4':
+        print(f"Result: {divide(num1, num2)}")
+    else:
+        print("Invalid input. Please choose a valid operation.")
+
+if __name__ == "__main__":
+    main()
