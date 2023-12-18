cf54f82d58fede76df2b9b7a38bdf627b9eda53c
laveezah-noor <laveezahnoor@gmail.com>
2023-12-18 19:55:53 +0500

    flake8 format

--- app.py
+++ app.py
@@ -3,17 +3,21 @@
 def add(x, y):
     return x + y
 
+
 def subtract(x, y):
     return x - y
 
+
 def multiply(x, y):
     return x * y
+
 
 def divide(x, y):
     if y != 0:
         return x / y
     else:
         return "Error: Cannot divide by zero!"
+
 
 def main():
     print("Welcome to Math Wizard!")
@@ -38,6 +42,7 @@ def main():
         print(f"Result: {divide(num1, num2)}")
     else:
         print("Invalid input. Please choose a valid operation.")
+
 
 if __name__ == "__main__":
     main()
